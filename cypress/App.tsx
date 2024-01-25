90c56932d1fc522c6dafd570ff1a3c66a7d87543
Jefferson Ding <tyding2004@gmail.com>
2024-01-25 15:07:18 -0500

    quick fix

--- cypress/App.tsx
+++ cypress/App.tsx
@@ -10,24 +10,14 @@ import Web3Auth, {LOGIN_PROVIDER} from '@web3auth/react-native-sdk';
 import {ethers} from 'ethers';
 import * as thor from '@vechain/web3-providers-connex';
 import {Framework} from '@vechain/connex-framework';
-import {Driver, SimpleWallet, SimpleNet} from '@vechain/connex-driver';
+import {Driver, SimpleWallet} from '@vechain/connex-driver';
+import {SimpleNet} from './custom_modules/@vechain/dist/simple-net';
 
 import Header from './components/Header';
 
 // Smart contract details
 const NODE_URL = 'https://sync-testnet.vechain.org';
 //import CONTRACT_ABI from './src/cypress_abi.json';
-
-const CONTRACT_ADDRESS = '0x0000000000000000000000000000456e65726779';
-const ABI = [
-  {
-    name: 'totalSupply',
-    inputs: [],
-    outputs: [{internalType: 'uint256', name: 'vtho', type: 'uint256'}],
-    stateMutability: 'view',
-    type: 'function',
-  },
-];
 
 // Web3Auth details
 const WEB3AUTH_CLIENTID =
@@ -65,10 +55,11 @@ const Button = ({children, onPress, color}: any): React.JSX.Element => {
 const App = (): React.JSX.Element => {
   const [wallet, setWallet] = useState<SimpleWallet>(new SimpleWallet());
   const [wallets, setWallets] = useState<Key[]>([]);
-  const [provider, setProvider] = useState<ethers.providers.Web3Provider>();
+  const [provider, setProvider] = useState<any>();
   const [account, setAccount] = useState<string>('Not Connected');
   const [balance, setBalance] = useState<string>('0 VET');
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const [contract, setContract] = useState<any>();
 
   const web3auth = new Web3Auth(WebBrowser, EncryptedStorage, {
     clientId: WEB3AUTH_CLIENTID, // Get your Client ID from the Web3Auth Dashboard
@@ -79,26 +70,37 @@ const App = (): React.JSX.Element => {
     web3auth.init();
   }, [web3auth]);
 
-  const updateBalance = () => {};
+  const updateBalance = () => {
+    if (provider) {
+      provider.getBalance(wallets[0].address).then((balance: any) => {
+        setBalance(ethers.utils.formatEther(balance) + ' VET');
+      });
+    }
+  };
 
   const initEthers = () => {
-    Driver.connect(new SimpleNet('https://testnet.vecha.in/')).then(driver => {
-      const connexObj = new Framework(driver);
-      const provider = thor.ethers.modifyProvider(
-        new ethers.providers.Web3Provider(
-          new thor.Provider({connex: connexObj}),
-        ),
-      );
-      setProvider(provider);
-      provider.ready.then(() => {
-        console.log('provider ready');
-      });
-      //ethersProvider
-      //.getBalance('0x08da8b8f2b1a19119e442cb7742d842a984677a3')
-      //.then(balance => {
-      //setBalance(ethers.utils.formatEther(balance));
-      //});
-    });
+    const CONTRACT_ADDRESS = '0x0000000000000000000000000000456e65726779';
+    const ABI = [
+      {
+        name: 'totalSupply',
+        inputs: [],
+        outputs: [{internalType: 'uint256', name: 'vtho', type: 'uint256'}],
+        stateMutability: 'view',
+        type: 'function',
+      },
+    ];
+    Driver.connect(new SimpleNet('https://testnet.vecha.in/')).then(
+      async driver => {
+        const connexObj = new Framework(driver);
+        const ethProvider = thor.ethers.modifyProvider(
+          new ethers.providers.Web3Provider(
+            new thor.Provider({connex: connexObj}),
+          ),
+        );
+        setProvider(ethProvider);
+        setContract(new ethers.Contract(CONTRACT_ADDRESS, ABI, ethProvider));
+      },
+    );
   };
 
   const handleLogin = async () => {
@@ -118,7 +120,13 @@ const App = (): React.JSX.Element => {
     setBalance('0 VET');
   };
 
-  const handleContract = async () => {};
+  const handleContract = async () => {
+    if (!provider || !contract) {
+      return;
+    }
+    const totalSupply = await contract.totalSupply();
+    console.log(totalSupply);
+  };
 
   return (
     <SafeAreaView style={tw`bg-white`}>
